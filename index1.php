ce21a3c2a869ca2066b43cc0e4e599b5235c4168
BastyMing <bmignolet2016@alu.uct.cl>
2016-10-07 10:34:52 -0300

    furmulario
    mmm

---
+++ index1.php
@@ -0,0 +1,36 @@
+ <html>
+ 	<head>
+ 		<title>
+			productos
+		</title>
+ 	</head>
+ 	<body>
+ 		<h2>
+ 			formulario
+ 		</h2>
+		<form name="productos" action="index.php">
+ 			<fieldset>
+ 				<legend>productos</legend>
+ 				<label for="proveedor">Proveedor</label>
+ 				<select id="proveedor" name="proveedor">
+
+ 				</select>
+ 				<label for="Nombre">Nombre</label>
+ 				<input type="text" name="Nombre" value=""/>
+ 				<br><br>
+ 				<label for="precios">precios</label>
+ 				<input type="text" name="precios" value=""/>
+ 				<br><br>
+ 				<label for="Stock">Stock</label>
+ 				<input type="text" name="Stock" value=""/>
+ 				<br><br>
+ 				<label for="Fecha">Fecha</label>
+ 				<input type="date" name="Fecha" value=""/>
+ 				<br>
+ 				<center>
+ 					<button name="Guardar">Guardar</button>
+ 				</center>
+ 			</fieldset>
+ 		</form>
+ 	</body>
+ </html>
