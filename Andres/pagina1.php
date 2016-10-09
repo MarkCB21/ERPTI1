ee4c71f65fa6152b89c61b14903c612157dd7cd6
DarkAnfi <aflores2015@alu.uct.cl>
2016-10-08 21:04:04 -0300

    Select Dependiente y Dinamico
    Ingenié una forma de hacer un select dependiente que a la vez sea
    dinamico.
    
    en style.css agregué el estado hover de los botones.

--- Andres/pagina1.php
+++ Andres/pagina1.php
@@ -8,30 +8,49 @@
   <meta charset='utf-8'>
   <title></title>
   <link href="../style/style.css" rel="stylesheet" type="text/css">
+  <?php
+    $con1 = mysql_query("SELECT ID_Prov, Nombre_Compania FROM proveedores");
+    while($fila1 = mysql_fetch_object($con1))
+    {
+      $Nombre_Compania = "$fila1->Nombre_Compania";
+      $ID_Prov = "$fila1->ID_Prov";
+      echo "<div id='$Nombre_Compania' hidden=False>\n";
+      $con2 = mysql_query("SELECT Nombre FROM productos WHERE ID_Prov = '$ID_Prov'");
+      while($fila2 = mysql_fetch_object($con2))
+      {
+        $Nombre_Producto = "$fila2->Nombre";
+        echo "<option value='$Nombre_Producto'>$Nombre_Producto</option>\n";
+      }
+      echo "</div>\n";
+    }
+  ?>
+  <script type="text/javascript">
+  function actdiv()
+  {
+    document.getElementById('nombre_producto').innerHTML = document.getElementById(document.getElementById('nombre_compania').value).innerHTML
+  }
+  </script>
   </head>
-  <body>
+  <body onload="actdiv()">
     <form id="Orden_De_Compra" action='pagina2.php' method='POST'>
     <fieldset>
       <legend>Orden de Compra</legend>
       <div>
-        <label class="form-label-large" for="nombre_producto">Nombre Producto</label>
-        <select class="form-control" name='nombre_producto' id='nombre_producto'>
+        <label class="form-label-large" for="nombre_compania">Nombre Compañia</label>
+        <select class="form-control" name='nombre_compania' id='nombre_compania' onchange="actdiv()">
           <?php  
-          $con1 = mysql_query("SELECT ID_Prov, Nombre_Compania FROM proveedores");
-          while($fila1 = mysql_fetch_object($con1))
+          $con = mysql_query("SELECT Nombre_Compania FROM proveedores");
+          while($fila = mysql_fetch_object($con))
           {
-            $Nombre_Compania = "$fila1->Nombre_Compania";
-            $ID_Prov = "$fila1->ID_Prov";
-            echo "<optgroup label='$Nombre_Compania'>\n";
-            $con2 = mysql_query("SELECT Nombre FROM productos WHERE ID_Prov = '$ID_Prov'");
-            while($fila2 = mysql_fetch_object($con2))
-            {
-              $Nombre_Producto = "$fila2->Nombre";
-              echo "<option value='$Nombre_Producto'>$Nombre_Producto</option>\n";
-            }
-            echo "</optgroup>\n";
+            $Nombre_Compania = "$fila->Nombre_Compania";
+            echo "<option value='$Nombre_Compania'>$Nombre_Compania</option>\n";
           }
           ?>
+        </select>
+      </div>
+      <div >
+        <label class="form-label-large" for="nombre_producto">Nombre Producto</label>
+        <select class="form-control" name='nombre_producto' id='nombre_producto'>
         </select>
       </div>
       <div>
