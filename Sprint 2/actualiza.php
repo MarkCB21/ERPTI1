ba990a55b1ba8bb5b7c25beb1e76b5c748c3786f
Maxteren <eayala2016@alu.uct.cl>
2016-10-27 17:24:36 -0300

    Actualiza producto

--- Sprint 2/actualiza.php
+++ Sprint 2/actualiza.php
@@ -7,13 +7,14 @@
 				$Precio_Unitario=$_GET['Precio_Unitario'];
 				echo $Precio_Unitario;
 				$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-				$con = "Update table productos(Precio_Unitario,Fecha_Modificacion,Medida) values(".$Precio_Unitario.",getdate(),".$Medida.") where ID_Prod=".$ID_Prod;
-				$result = mysqli_query($link,$con);
+				$con = "Update productos set(Precio_Unitario=".$Precio_Unitario.",Fecha_Modificacion=getdate(),Medida=".$Medida.") where ID_Prod=".$ID_Prod.";";
+				echo $con;
+				echo "<br>".$ID_Prod;
+				mysqli_query($link,$con);
 				mysqli_close($link);
-				header("location: Modulo_Productos.php");
-				}
+				#header("location: Modulo_Productos.php");
 			}
 		}
 	}
 
-?>
+?>s
