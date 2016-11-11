06e42e9c11375cb0a3d0a0cc0a5a7feceb5ac2ba
riquelme1 <matikang@hotmail.com>
2016-11-11 12:13:54 -0400

    Pagina para agregar proveedores

--- Sprint final paginas/Agregar_Proveedor.php
+++ Sprint final paginas/Agregar_Proveedor.php
@@ -16,6 +16,40 @@ include "aside.php";?>
 					<input id="Nombre_Compania" name="Nombre_Compania" class="form-control" type="text">
 				</div>
 				<div>
+					<label for="Nombre_Local" class="form-label">Nombre Local</label>
+					<input id="Nombre_Local" name="Nombre_Local" class="form-control" type="text">
+				</div>
+				<div>
+					<label for="ID_Rut" class="form-label">Rut Compania</label>
+					<input id="ID_Rut" name="ID_Rut" class="form-control" type="text">
+				</div>
+				<div>
+					<label for="Direccion" class="form-label">Direccion</label>
+					<input id="Direccion" name="Direccion" class="form-control" type="text">
+				</div>
+				<div>
+					<label for="region" class="form-label">Region</label>
+					<select id="region" name="region" class="form-control" type="text">
+					<?php 
+					for ($i=0; $i < count($region_ID_Region); $i++)
+					{ 
+						echo "<option value='$region_ID_Region[$i]'>$region_Nombre_Region[$i]</option>";
+					}
+					?>
+					</select>
+				</div>
+				<div>
+					<label for="comuna" class="form-label">Comuna</label>
+					<select id="comuna" name="comuna" class="form-control" type="text">
+					<?php 
+					for ($i=0; $i < count($comuna_ID_Comuna); $i++)
+					{ 
+						echo "<option value='$comuna_ID_Comuna[$i]'>$comuna_Nombre_Comuna[$i]</option>";
+					}
+					?>
+					</select>
+				</div>
+				<div>
 					<label for="Tipo_Proveedor" class="form-label">Tipo Proveedor</label>
 					<select id="Tipo_Proveedor" name="Tipo_Proveedor" class="form-control">
 					<?php 
@@ -25,10 +59,6 @@ include "aside.php";?>
 					}
 					?>
 					</select>
-				</div>
-				<div>
-					<label for="ID_Rut" class="form-label">Rut</label>
-					<input id="ID_Rut" name="ID_Rut" class="form-control" type="text">
 				</div>
 				<div>
 					<label for="Nombre_C" class="form-label">Nombre Completo</label>
@@ -43,24 +73,16 @@ include "aside.php";?>
 					<input id="Apellido_M" name="Apellido_M" class="form-control" type="text">
 				</div>
 				<div>
+					<label for="rut" class="form-label">Rut Proveedor</label>
+					<input id="rut" name="rut" class="form-control" type="text">
+				</div>
+				<div>
 					<label for="Correo" class="form-label">Correo</label>
 					<input id="Correo" name="Correo" class="form-control" type="text">
 				</div>
 				<div>
 					<label for="Telefono" class="form-label">Telefono</label>
 					<input id="Telefono" name="Telefono" class="form-control" type="text">
-				</div>	
-				<div>
-					<label for="Direccion" class="form-label">Direccion</label>
-					<input id="Direccion" name="Direccion" class="form-control" type="text">
-				</div>
-				<div>
-					<label for="Comuna" class="form-label">Comuna</label>
-					<input id="Comuna" name="Comuna" class="form-control" type="text">
-				</div>
-				<div>
-					<label for="Nombre_Local" class="form-label">Nombre Local</label>
-					<input id="Nombre_Local" name="Nombre_Local" class="form-control" type="text">
 				</div>
 				<center>
 					<input class="btn" type="submit" value="Crear">
--- Sprint final paginas/Programa_Agregar_Proveedor.php
+++ Sprint final paginas/Programa_Agregar_Proveedor.php
@@ -1,47 +1,49 @@
 <?php
 include 'constantes.php';
 
-if (isset($_POST['Nombre_Compania']) && isset($_POST['Tipo_Proveedor']) && isset($_POST['ID_Rut']) && isset($_POST['Nombre_C']) && isset($_POST['Apellido_P']) && isset($_POST['Apellido_M']) && isset($_POST['Correo']) && isset($_POST['Telefono']) && isset($_POST['Direccion']) && isset($_POST['Nombre_Local']) && isset($_POST['Comuna']))
+if (isset($_POST['Nombre_Compania']) && isset($_POST['Nombre_Local']) && isset($_POST['ID_Rut']) && isset($_POST['Direccion']) && isset($_POST['region']) && isset($_POST['comuna']) && isset($_POST['Tipo_Proveedor']) && isset($_POST['Nombre_C']) && isset($_POST['Apellido_P']) && isset($_POST['Apellido_M']) && isset($_POST['rut']) && isset($_POST['Correo']) && isset($_POST['Telefono']))
 {	
 	$Nombre_Compania=$_POST['Nombre_Compania'];
-	$Tipo_Proveedor=$_POST['Tipo_Proveedor'];
+	$Nombre_Local=$_POST['Nombre_Local'];
 	$ID_Rut=$_POST['ID_Rut'];
+	$Direccion=$_POST['Direccion'];
+	$Comuna=$_POST['comuna'];
+	$Tipo_Proveedor=$_POST['Tipo_Proveedor'];
 	$Nombre_C=$_POST['Nombre_C'];
 	$Apellido_P=$_POST['Apellido_P'];
 	$Apellido_M=$_POST['Apellido_M'];
+	$Rut=$_POST['rut'];
 	$Correo=$_POST['Correo'];
 	$Telefono=$_POST['Telefono'];
-	$Direccion=$_POST['Direccion'];
-	$Nombre_Local=$_POST['Nombre_Local'];
-	$Comuna=$_POST['Comuna'];
+	echo $Comuna;
 
 	$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-
-	$con = "SELECT ID_Comuna FROM comuna WHERE Nombre_Comuna='$Comuna';";
-	$result = mysqli_query($link,$con);
-	$row = mysqli_fetch_object($result);
-	$ID_Comuna = "$row->ID_Comuna";
-
-	$con = "SELECT ID_Direccion FROM direccion WHERE Direccion='$Direccion' AND Nombre_Local='$Nombre_Local' AND ID_Comuna='$ID_Comuna';";
-	$result = mysqli_query($link,$con);
-	if(mysqli_num_rows($result)<1)
-	{	
-		$con = "INSERT INTO direccion VALUES(NULL,'$Direccion','$Nombre_Local','$ID_Comuna');";
-		mysqli_query($link,$con);
-		echo "$con<br>";
-	}
-	$con = "SELECT ID_Direccion FROM direccion WHERE Direccion='$Direccion' AND Nombre_Local='$Nombre_Local' AND ID_Comuna='$ID_Comuna';";
-	$result = mysqli_query($link,$con);
-	$row = mysqli_fetch_object($result);
-	$ID_Direccion = intval($row->ID_Direccion);
-	echo "$con<br>";
-	echo "$ID_Direccion<br>";
-
-	$con = "INSERT INTO proveedores VALUES(NULL,'$Nombre_Compania','$Tipo_Proveedor','$ID_Rut','$Nombre_C','$Apellido_P','$Apellido_M','$Correo','$Telefono','$ID_Direccion');";
+	$con="INSERT INTO direccion VALUES(NULL,'$Direccion', '$Nombre_Local', '$Comuna');";
+	mysqli_query($link, $con);
+	$sql=mysqli_query($link, $con);
+	$con="SELECT ID_Direccion FROM direccion WHERE Direccion='$Direccion' AND Nombre_Local='$Nombre_Local';";
+	if($result=mysqli_query($link, $con)){
+		$ID_Direccion=[];
+			while ($row = mysqli_fetch_object($result)){
+				array_push($ID_Direccion, $row->ID_Direccion);
+			}
+		mysqli_free_result($result);
+		}
+	$con="INSERT INTO datos VALUES(NULL,'$Rut','$Nombre_C','$Apellido_P','Apellido_M','$Correo','$Telefono','$ID_Direccion[0]');";
+	mysqli_query($link, $con);
+	$sql=mysqli_query($link, $con);
+	$con="SELECT ID_Datos FROM datos WHERE rut='$Rut';";
+	if($result=mysqli_query($link, $con)){
+		$ID_Datos=[];
+			while ($row = mysqli_fetch_object($result)){
+				array_push($ID_Datos, $row->ID_Datos);
+			}
+		mysqli_free_result($result);
+		}
+	$con="INSERT INTO proveedores VALUES(NULL,'$Nombre_Compania','$Tipo_Proveedor','$ID_Rut', '$ID_Datos[0]');";
 	echo $con;
-	mysqli_query($link,$con);
-
+	$sql=mysqli_query($link, $con);
 	mysqli_close($link);
-}
+	}
 header("location: Modulo_Proveedores.php");
 ?>
