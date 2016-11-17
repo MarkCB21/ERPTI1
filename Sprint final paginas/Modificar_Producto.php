eca37174195e25f889d9aefc05cb0e95370dff91
riquelme1 <matikang@hotmail.com>
2016-11-16 22:38:28 -0400

    Revert "C0242"
    This reverts commit 6cc7ecf97a1bba2251f4b0a10b1e1e44841382ab.

--- Sprint final paginas/Modificar_Producto.php
+++ Sprint final paginas/Modificar_Producto.php
@@ -29,7 +29,11 @@ else
 				</div>
 				<div>
 					<label for='Medida' class='form-label'>Nombre Producto</label>
-					<input id='Medida' class='form-control' type='text' value='".$productos_Descripcion[intval($ID_Prod)-1]."' readonly>
+					<input id='Medida' class='form-control' type='text' value='".$productos_Nombre[intval($ID_Prod)-1]."' readonly>
+				</div>
+				<div>
+					<label for='Medida' class='form-label'>Medida</label>
+					<input id='Medida' name='Medida' class='form-control' type='text' value='".$productos_Medida[intval($ID_Prod)-1]."'>
 				</div>
 				<div>
 					<label for='Precio_Unitario' class='form-label'>Precio por Medida</label>
--- Sprint final paginas/Programa_Modificar_Producto.php
+++ Sprint final paginas/Programa_Modificar_Producto.php
@@ -1,13 +1,16 @@
 <?php include "tablas.php"; 
-	if(isset($_GET['Precio_Unitario'])){
-		if(isset($_GET['ID_Prod'])){
-			$ID_Prod=$_GET['ID_Prod'];
-			$Precio_Unitario=$_GET['Precio_Unitario'];
-			$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-			$con = "UPDATE productos SET Precio_Unitario = '$Precio_Unitario' WHERE ID_Prod = '$ID_Prod'";
-			mysqli_query($link,$con);
-			mysqli_close($link);
-			header("location: Modulo_Productos.php");
+	if(isset($_GET['Medida'])){
+		if(isset($_GET['Precio_Unitario'])){
+			if(isset($_GET['ID_Prod'])){
+				$ID_Prod=$_GET['ID_Prod'];
+				$Medida=$_GET['Medida'];
+				$Precio_Unitario=$_GET['Precio_Unitario'];
+				$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+				$con = "UPDATE productos SET Precio_Unitario = '$Precio_Unitario', Medida = '$Medida' WHERE ID_Prod = '$ID_Prod'";
+				mysqli_query($link,$con);
+				mysqli_close($link);
+				header("location: Modulo_Productos.php");
+			}
 		}
 	}
 
