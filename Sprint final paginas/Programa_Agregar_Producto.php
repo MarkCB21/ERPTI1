ca499d4d30e6dbde82e6f7072faec4ea8f434c05
ItsmeSonny <xAleeh@live.cl>
2016-11-18 11:29:27 -0400

    Update Programa_Agregar_Producto.php

--- Sprint final paginas/Programa_Agregar_Producto.php
+++ Sprint final paginas/Programa_Agregar_Producto.php
@@ -1,16 +1,16 @@
 <?php
 include 'constantes.php';
-if(isset($_POST['ID_Prov']) && isset($_POST['ID_Categoria']) && isset($_POST['Nombre']) && isset($_POST['Medida']) && isset($_POST['Precio_Unitario']))
+if(isset($_POST['ID_Prov']) && isset($_POST['ID_Categoria']) && isset($_POST['Descripcion']) && isset($_POST['Precio_Unitario']) && isset($_POST['Fecha_Agregado']))
 {
 	$ID_Prov=$_POST['ID_Prov'];
 	$ID_Categoria=$_POST['ID_Categoria'];
-	$Nombre=$_POST['Nombre'];
-	$Medida=$_POST['Medida'];
+	$Descripcion=$_POST['Descripcion'];
 	$Precio_Unitario=$_POST['Precio_Unitario'];
+	$Fecha_Agregado=$_POST['Fecha_Agregado'];
 
 	$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
-	$query = "INSERT INTO productos VALUES(NULL,'$ID_Categoria','$Nombre','$Precio_Unitario',CURDATE(),CURDATE(),'$Medida','$ID_Prov');";
+	$query = "INSERT INTO productos VALUES(NULL,'$ID_Categoria','$Descripcion','$Precio_Unitario','$Fecha_Agregado','$ID_Prov');";
 	mysqli_query($link,$query);
 
 	mysqli_close($link);
